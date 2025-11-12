1a033babe36af72cf7a4abd21e988690ad8a1c3c
hoanganhvu19052007 <hoangvu2007hg@gmail.com>
2025-11-13 01:02:22 +0700

    Add files via upload

--- SESSION15_C/BTVN09.c
+++ SESSION15_C/BTVN09.c
@@ -1,33 +1,34 @@
-#include<stdio.h>
-int deleteElement(int arr[],int size,int index){
-	int i;
-	for(i = index;i < size; i++){
-		arr[i] = arr[i+1];
-	}
-	return size- 1;
-}
-int removeDuplicates(int arr[],int size){
-		int newSize = size ;
-		int i, j;
-		for(i=0;i<newSize;i++){
-			for(j=i+1;j<newSize;j++){
-				if(arr[i] == arr[j]){
-				newSize = deleteElement(arr,newSize,j);
-				j--;
-			}
-		}
-	}
-	return newSize ;
-}
-int main(){
-	int arr[] = {1,3,3,2,4,2,5};
-	int size = sizeof(arr) / sizeof(arr[0]);
-	int i;
-	size = removeDuplicates(arr,size);
+#include <stdio.h>
 
-	printf("Mang sau khi xoa cac phan tu trung lap: ");
-	for(i=0;i<size;i++){
-		printf("%d ", arr[i]);
-	}
-	return 0;
+int findLeastFrequency(int arr[], int size) {
+    int minFreq = size + 1;   
+    int result = arr[0];      
+	int i, j;
+    for (i = 0; i < size; i++) {
+        int freq = 1;  
+
+        for (j = i + 1; j < size; j++) {
+            if (arr[j] == arr[i]) {
+                freq++;
+            }
+        }
+
+        if (freq < minFreq) {
+            minFreq = freq;
+            result = arr[i];
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    int arr[] = {1, 3, 3, 2, 4, 2, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int least = findLeastFrequency(arr, size);
+
+    printf("Phan tu xuat hien it nhat la: %d\n", least);
+
+    return 0;
 }
